d43d9734047a3df275a66bf9578a872364e60f06
Nacho <nacho@FEIT4533.local>
2015-05-19 12:07:49 +1000

    Initial upload

---
+++ YandereVIEW/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  YandereVIEW
+//
+//  Created by Nacho on 23/04/2015.
+//  Copyright (c) 2015 Advance Analytics Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) ViewController *viewController;
+
+
+
+@end
+
